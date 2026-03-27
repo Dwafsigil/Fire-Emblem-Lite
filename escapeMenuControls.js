48c9d1d69f4e1e7369c87904bd2588c6c08e2478
Dwafsigil <jtu02@jaguar.tamu.edu>
2026-03-27 02:45:27 -0500

    Added a bit more functionality to the escape menu. Can now scroll through options and select the controls menu to view the controls.

--- escapeMenuControls.js
+++ escapeMenuControls.js
@@ -1,15 +1,43 @@
+import { Phase } from "./state.js";
+
 export function escapeMenuControls(ui, state, gates) {
   document.addEventListener("keydown", (e) => {
     e.preventDefault;
     console.log("Pressed");
-    if (e.code === "Escape") {
-      if (ui.escapeMenu.classList.contains("hidden")) {
-        ui.escapeMenu.classList.remove("hidden");
-      } else {
-        ui.escapeMenu.classList.add("hidden");
+    if (e.code === "Escape" && state.phase !== Phase.MENU) {
+      ui.escapeMenu.classList.toggle("hidden");
+      if (!ui.escapeMenu.classList.contains("hidden")) {
+        const buttons = ui.escapeMenu.querySelectorAll("button");
+        buttons[0].focus();
       }
     }
   });
-}
 
+  ui.escapeMenu.addEventListener("keydown", (e) => {
+    let active = document.activeElement;
+
+    let escapeMenuBtns = ui.escapeMenu.querySelectorAll("button");
+    const current = Number(active.dataset.index);
+    let next = current;
+
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      next = (current + 1) % escapeMenuBtns.length;
+    }
+
+    if (e.key === "ArrowUp") {
+      e.preventDefault();
+      next = (current - 1 + escapeMenuBtns.length) % escapeMenuBtns.length;
+    }
+
+    if (e.key === "z") {
+      if (current === 0) {
+        console.log("This is zero");
+        ui.controls.classList.toggle("hidden");
+      }
+    }
+
+    escapeMenuBtns[next].focus();
+  });
+}
 // Element.classList.contains()
